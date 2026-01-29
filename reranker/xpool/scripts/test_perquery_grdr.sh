bc464f7003ac3d894f17712980107e5b144d1c8f
JsonCodeMaker <zzc010220@gmail.com>
2026-01-29 14:01:28 +1000

    Update

--- reranker/xpool/scripts/test_perquery_grdr.sh
+++ reranker/xpool/scripts/test_perquery_grdr.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-DEVICE=1
+DEVICE=0
 
+## Setting 1: Inductive setting
+# MSRVTT
 VIDEOS_DIR="dataset/msrvtt_data/MSRVTT_Videos"
 CHECKPOINT="reranker/xpool/ckpt/msrvtt9k_model_best.pth"
-CANDIDATE_FILE=${1:-"candidates/unimvp/msrvtt_videorqvae__c128l3_20_candidates_t2.json"}
-echo "Running candidate reranking mode with: $CANDIDATE_FILE"
+CANDIDATE_FILE="candidates/msrvtt_c128l3_100_candidates_t1.json"
 CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
     --dataset_name MSRVTT \
     --expanded_pool \
@@ -17,10 +18,9 @@ CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
     --seed 42
 
 # ACTNET
-VIDEOS_DIR="/data2/uqzzha35/VideoRetrieval/ActivityNet/Activity_Videos"
+VIDEOS_DIR="dataset/ActivityNet/Activity_Videos"
 CHECKPOINT="reranker/xpool/ckpt/actnet_model_best.pth"
-CANDIDATE_FILE=${1:-"candidates/actnet_videorqvae__c128l3_50_candidates_t2.json"}
-echo "Running candidate reranking mode with: $CANDIDATE_FILE"
+CANDIDATE_FILE="candidates/actnet_c128l3_100_candidates_t1.json"
 CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
     --dataset_name ACTNET \
     --expanded_pool \
@@ -29,14 +29,13 @@ CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
     --huggingface \
     --checkpoint ${CHECKPOINT} \
     --candidate_file ${CANDIDATE_FILE} \
-    --cache_dir reranker/xpool/video_features_cache/ACTNET \
+    --cache_dir reranker/xpool/video_features_cache/Xpool/ACTNET \
     --seed 42
 
 # DIDEMO
-VIDEOS_DIR="/data2/uqzzha35/VideoRetrieval/DiDeMo"
+VIDEOS_DIR="dataset/DiDeMo"
 CHECKPOINT="reranker/xpool/ckpt/didemo_model_best.pth"
-CANDIDATE_FILE=${1:-"candidates/didemo_videorqvae__c96l3_50_candidates_t2.json"}
-echo "Running candidate reranking mode with: $CANDIDATE_FILE"
+CANDIDATE_FILE="candidates/didemo_c96l3_100_candidates_t1.json"
 CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
     --dataset_name DIDEMO \
     --expanded_pool \
@@ -44,14 +43,13 @@ CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
     --huggingface \
     --checkpoint ${CHECKPOINT} \
     --candidate_file ${CANDIDATE_FILE} \
-    --cache_dir reranker/xpool/video_features_cache/DIDEMO \
+    --cache_dir reranker/xpool/video_features_cache/Xpool/DIDEMO \
     --seed 42
 
 # LSMDC
-VIDEOS_DIR="/data2/uqzzha35/VideoRetrieval/LSMDC/LSMDC_Videos"
+VIDEOS_DIR="dataset/LSMDC/LSMDC_Videos"
 CHECKPOINT="reranker/xpool/ckpt/lsmdc_model_best.pth"
-CANDIDATE_FILE=${1:-"candidates/lsmdc_videorqvae__c200l3_50_candidates_t2.json"}
-echo "Running candidate reranking mode with: $CANDIDATE_FILE"
+CANDIDATE_FILE="candidates/lsmdc_c200l3_100_candidates_t1.json"
 CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
     --dataset_name LSMDC \
     --expanded_pool \
@@ -59,5 +57,62 @@ CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
     --huggingface \
     --checkpoint ${CHECKPOINT} \
     --candidate_file ${CANDIDATE_FILE} \
-    --cache_dir reranker/xpool/video_features_cache/LSMDC \
+    --cache_dir reranker/xpool/video_features_cache/Xpool/LSMDC \
+    --seed 42
+
+## Setting 2: Full-corpus setting
+# MSRVTT
+VIDEOS_DIR="dataset/MSRVTT/MSRVTT_Videos"
+CHECKPOINT="reranker/xpool/ckpt/msrvtt9k_model_best.pth"
+CANDIDATE_FILE="candidates/msrvtt_c128l3_20_candidates_t2.json"
+CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
+    --dataset_name MSRVTT \
+    --expanded_pool \
+    --videos_dir ${VIDEOS_DIR} \
+    --huggingface \
+    --checkpoint ${CHECKPOINT} \
+    --candidate_file ${CANDIDATE_FILE} \
+    --cache_dir reranker/xpool/video_features_cache/Xpool/MSRVTT \
+    --seed 42
+
+# ACTNET
+VIDEOS_DIR="dataset/ActivityNet/Activity_Videos"
+CHECKPOINT="reranker/xpool/ckpt/actnet_model_best.pth"
+CANDIDATE_FILE="candidates/actnet_c128l3_50_candidates_t2.json"
+CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
+    --dataset_name ACTNET \
+    --expanded_pool \
+    --videos_dir ${VIDEOS_DIR} \
+    --huggingface \
+    --checkpoint ${CHECKPOINT} \
+    --candidate_file ${CANDIDATE_FILE} \
+    --cache_dir reranker/xpool/video_features_cache/Xpool/ACTNET \
+    --seed 42
+
+# DIDEMO
+VIDEOS_DIR="dataset/DiDeMo"
+CHECKPOINT="reranker/xpool/ckpt/didemo_model_best.pth"
+CANDIDATE_FILE="candidates/didemo_c96l3_50_candidates_t2.json"
+CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
+    --dataset_name DIDEMO \
+    --expanded_pool \
+    --videos_dir ${VIDEOS_DIR} \
+    --huggingface \
+    --checkpoint ${CHECKPOINT} \
+    --candidate_file ${CANDIDATE_FILE} \
+    --cache_dir reranker/xpool/video_features_cache/Xpool/DIDEMO \
+    --seed 42
+
+# LSMDC
+VIDEOS_DIR="dataset/LSMDC/LSMDC_Videos"
+CHECKPOINT="reranker/xpool/ckpt/lsmdc_model_best.pth"
+CANDIDATE_FILE="candidates/lsmdc_c200l3_20_candidates_t2.json"
+CUDA_VISIBLE_DEVICES="${DEVICE}" python reranker/xpool/test_perquery.py \
+    --dataset_name LSMDC \
+    --expanded_pool \
+    --videos_dir ${VIDEOS_DIR} \
+    --huggingface \
+    --checkpoint ${CHECKPOINT} \
+    --candidate_file ${CANDIDATE_FILE} \
+    --cache_dir reranker/xpool/video_features_cache/Xpool/LSMDC \
     --seed 42
